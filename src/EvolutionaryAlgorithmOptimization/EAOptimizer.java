380cfd1e8d6f8f7c022c448d0599b8880ee1f5a4
Maria <maria.kokshaikina@gmail.com>
2017-11-15 02:11:27 +0200

    add evolutionary optimizer class

---
+++ src/EvolutionaryAlgorithmOptimization/EAOptimizer.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EvolutionaryAlgorithmOptimization;
+
+import PetriObj.PetriObjModel;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author masha
+ */
+public abstract class EAOptimizer {
+        
+    private final PetriObjModel initialModel;
+    
+    private ArrayList<PetriObjModel> population;
+    private int populationSize;
+    private int generationsNumber;
+    
+    private double elitismProbability;
+    private double mutationProbability;
+    private double crossoverProbability;
+
+    /**
+     * Create optimization tool for PetriObjModel.
+     * Default population size = 10, generations number = 10. Set other values if needed.
+     * In order to use crossover method override it.
+     * @param initialModel
+     */
+    public EAOptimizer(PetriObjModel initialModel) {
+        this.initialModel = initialModel;
+        this.populationSize = 10;
+        this.generationsNumber = 10;
+    }
+
+    public PetriObjModel getInitialModel() {
+        return initialModel;
+    }    
+
+    public int getPopulationSize() {
+        return populationSize;
+    }
+
+    public void setPopulationSize(int populationSize) {
+        this.populationSize = populationSize;
+    }
+
+    public int getGenerationsNumber() {
+        return generationsNumber;
+    }
+
+    public void setGenerationsNumber(int generationsNumber) {
+        this.generationsNumber = generationsNumber;
+    }
+    
+    /**
+     * Set probabilities for elitism, mutation and crossover.
+     * @param elitismProbability
+     * @param mutationProbability
+     * @param crossoverProbability
+     * @throws Exception if sum of probabilities doesn't equal to 1.00
+     */
+    public void setProbabilities(double elitismProbability, double mutationProbability, double crossoverProbability) throws Exception{
+        if (elitismProbability + mutationProbability + crossoverProbability != 1){
+            throw new Exception("Sum of probabilities doesn't equal to 1.00");
+        }
+        this.elitismProbability = elitismProbability;
+        this.mutationProbability = mutationProbability;
+        this.crossoverProbability = crossoverProbability;
+    }
+    
+    private void generatePopulation(){
+        population = new ArrayList<>();
+        // TODO
+        for (int i = 0; i < populationSize; i++){
+            population.add(initialModel);
+        }
+    }
+    
+    private ArrayList<PetriObjModel> elitism(){
+        ArrayList<PetriObjModel> elite_individuums = new ArrayList<>();
+        int number = (int) (population.size() * elitismProbability);
+        // TODO
+        for (int i = 0; i < number; i++){
+            elite_individuums.add(initialModel);
+        }
+        return elite_individuums;
+    }
+    
+    private ArrayList<PetriObjModel> mutation(){
+        ArrayList<PetriObjModel> mutated_individuums = new ArrayList<>();
+        int number = (int) (population.size() * mutationProbability);
+        // TODO
+        for (int i = 0; i < number; i++){
+            mutated_individuums.add(initialModel);
+        }
+        return mutated_individuums;
+    }
+    
+    protected abstract ArrayList<PetriObjModel> crossover();
+    
+    private double fitness_function(PetriObjModel model){
+        // TODO
+        return 0;
+    }
+    
+    public PetriObjModel evolve(){
+        generatePopulation();
+        
+        for (int i = 0; i < generationsNumber; i++){
+            
+            ArrayList<PetriObjModel> elite_individuums = elitism();
+//            System.out.format("Elite size: %d\n", elite_individuums.size());
+            System.out.format("Generation: %d. Best result: %f\n", i, fitness_function(elite_individuums.get(0)));
+            
+            ArrayList<PetriObjModel> mutated_individuums = mutation();
+//            System.out.format("Mutated size: %d\n", mutated_individuums.size());
+            
+            ArrayList<PetriObjModel> crossover_individuums = null;
+            if (crossoverProbability != 0){
+                crossover_individuums = crossover();
+//                System.out.format("Crossover size: %d\n", crossover_individuums.size());
+            }
+            
+            population = new ArrayList<>();
+            population.addAll(elite_individuums);
+            population.addAll(mutated_individuums);
+            if (crossoverProbability != 0){
+                population.addAll(crossover_individuums);
+            }
+//            System.out.format("Population size: %d\n", population.size());
+        }
+        
+        return population.get(0);
+    }
+    
+}
---
+++ src/EvolutionaryAlgorithmOptimization/EAOptimizerUsingExample.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EvolutionaryAlgorithmOptimization;
+
+import LibTest.TestPetriObjPaint;
+import PetriObj.ExceptionInvalidNetStructure;
+import PetriObj.ExceptionInvalidTimeDelay;
+import PetriObj.PetriObjModel;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author masha
+ */
+public class EAOptimizerUsingExample {
+    public static void main(String[] args) throws ExceptionInvalidTimeDelay, ExceptionInvalidNetStructure, Exception{
+        PetriObjModel model = TestPetriObjPaint.getModel();
+        
+        EAOptimizer optimizer = new EAOptimizer(model) {
+            @Override
+            protected ArrayList<PetriObjModel> crossover() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }            
+        };
+        optimizer.setPopulationSize(10);
+        optimizer.setGenerationsNumber(10);
+        optimizer.setProbabilities(0.2, 0.8, 0);
+        
+        PetriObjModel best = optimizer.evolve();
+        
+    }
+}
