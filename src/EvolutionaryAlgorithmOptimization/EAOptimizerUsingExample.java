04e704a65cf5544c595be7b8e906926f77fea988
Maria <maria.kokshaikina@gmail.com>
2017-11-28 01:26:38 +0200

    refactor

--- src/EvolutionaryAlgorithmOptimization/EAOptimizerUsingExample.java
+++ src/EvolutionaryAlgorithmOptimization/EAOptimizerUsingExample.java
@@ -9,32 +9,32 @@ import LibTest.TestPetriObjPaint;
 import PetriObj.ExceptionInvalidNetStructure;
 import PetriObj.ExceptionInvalidTimeDelay;
 import PetriObj.PetriObjModel;
+
 import java.util.ArrayList;
 
 /**
- *
  * @author masha
  */
 public class EAOptimizerUsingExample {
-    public static void main(String[] args) throws ExceptionInvalidTimeDelay, ExceptionInvalidNetStructure, Exception{
+    public static void main(String[] args) throws ExceptionInvalidTimeDelay, ExceptionInvalidNetStructure, Exception {
         PetriObjModel model = TestPetriObjPaint.getModel();
-        
+
         EAOptimizer optimizer = new EAOptimizer(model) {
             @Override
             protected ArrayList<PetriObjModel> crossover() {
                 throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }            
+            }
 
             @Override
-            public double fitness_function(PetriObjModel model) {
+            public double fitnessFunction(PetriObjModel model) {
                 return 0;
             }
         };
         optimizer.setPopulationSize(10);
         optimizer.setGenerationsNumber(10);
-        optimizer.setProbabilities(0.2, 0.8, 0, 0.75);
-        
+        optimizer.setProbabilities(0.2, 0.8, 0, 0.5);
+
         PetriObjModel best = optimizer.evolve();
-        
+
     }
 }
